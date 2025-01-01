05195af29ba7d25f858bb729277a2681403d53c3
Muhammad Rizwan <dua44@hotmail.com>
2025-01-01 14:54:25 +0500

    -

--- src/db/index.js
+++ src/db/index.js
@@ -4,11 +4,8 @@ import { DB_Name } from '../constants.js'
 const connectDB = async () => {
     try {
 
-        //console.log(process.env.MONGODB_URL)
-
         const dbURL = `${process.env.MONGODB_URL}/${DB_Name}`;
-
-        console.log("Connecting to database at", dbURL);
+        console.log("Connecting to database at", DB_Name);
         const connectionInstance = await mongoose.connect(dbURL);
         console.log(`MongoDB connected DB Host:${connectionInstance.connection.host}`);
 
