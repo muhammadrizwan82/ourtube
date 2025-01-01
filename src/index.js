db31ef041beeffa0d4ddd9619a3791e5dc1af53a
Muhammad Rizwan <dua44@hotmail.com>
2025-01-01 16:15:51 +0500

    utils

--- src/index.js
+++ src/index.js
@@ -1,40 +1,18 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
-import express from 'express';
+import { app } from './app.js';
+
 
 dotenv.config({
   path: './.env.sample'
 });
 
-//dotenv.config()
-
-const app = express();
-
-connectDB();
-
-/*
-(async () => {
-  try {
-    const dbURL = `${process.env.MONGODB_URL}/${DB_NAME}`;
-    console.log("Connecting to database at", dbURL);
-
-    await mongoose.connect(dbURL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("Connected to database");
-
-    app.on('error', (error) => {
-      console.error('Server error:', error);
-      throw error;
-    });
-
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening on port ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error(`Error in connecting db:`, error);
-    throw error;
-  }
-})();
-*/
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is listening at PORT:${process.env.PORT}`);
+    })
+  })
+  .catch((err) => {
+    console.log('Mongodb Connection Failed !!!', err)
+  })
